287e16a14c256afdf8f44211a151657117306334
Jesse Leite <jesseleite@gmail.com>
2015-02-18 15:12:07 -0500

    Update after.sh

--- after.sh
+++ after.sh
@@ -4,7 +4,7 @@ apt-get install zsh -y
 # Clone Oh-My-Zsh.
 git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
 
-# Copy Robby's .zshrc template over.
+# Copy Robby's .zshrc over as .zshrc-robby for reference purposes only.
 cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc-robby
 
 # Clone my .zshrc and theme.
