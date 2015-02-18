5693fda8c235a76e2ed14c13c709d383b0c1911e
Jesse Leite <jesseleite@gmail.com>
2015-02-18 14:08:28 -0500

    Changed folder name.

--- after.sh
+++ after.sh
@@ -11,7 +11,7 @@ git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
 cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
 
 # Clone my files.
-git clone git@github.com:JerseyMilker/HomesteadAfter.git /home/vagrant/.homesteadAfter
+git clone git@github.com:JerseyMilker/HomesteadAfter.git /home/vagrant/.homestead-after
 
 # Set default shell.
 chsh -s /usr/bin/zsh vagrant
