4585efa40422894e7ae40b9619302f7d5cbef511
Jesse Leite <jesseleite@gmail.com>
2015-02-18 18:41:48 -0500

    Update after.sh

--- after.sh
+++ after.sh
@@ -7,15 +7,17 @@ git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
 # Copy Robby's .zshrc over as .zshrc-robby for reference purposes only.
 cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc-robby
 
-# Clone my .zshrc and theme.
+# Clone HomesteadAfter files.
 git clone git://github.com/JerseyMilker/HomesteadAfter.git /home/vagrant/.homestead-after
-cp /home/vagrant/.homestead-after/jersey-homestead.zsh-theme /home/vagrant/.oh-my-zsh/themes
-cp /home/vagrant/.homestead-after/.zshrc /home/vagrant/.zshrc
 
-# Clone my .vimrc.
+# Copy out .zshrc and theme.
+cp /home/vagrant/.homestead-after/.zshrc /home/vagrant/.zshrc
+cp /home/vagrant/.homestead-after/jersey-homestead.zsh-theme /home/vagrant/.oh-my-zsh/themes
+
+# Copy out .vimrc.
 cp /home/vagrant/.homestead-after/.vimrc /home/vagrant/.vimrc
 
-# Clone .gitconfig and setup git user.
+# Copy out .gitconfig and setup git user.
 cp /home/vagrant/.homestead-after/.gitconfig /home/vagrant/.gitconfig
 #git config --global user.name "John Madden"
 #git config --global user.email "john@example.com"
@@ -26,4 +28,5 @@ chsh -s /usr/bin/zsh vagrant
 # Final message.
 echo '*****'
 echo 'Done installing HomesteadAfter extras <3'
-echo 'Note: Setup git user via gituser alias!'
+echo 'Note: Git user has not been set up!'
+echo 'You can set up git user via gituser alias.'
