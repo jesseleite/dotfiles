701dca12e88042e9572be61e9b22c69547cf29a5
Jesse Leite <jesseleite@gmail.com>
2015-02-18 14:19:38 -0500

    Update after.sh

--- after.sh
+++ after.sh
@@ -8,10 +8,12 @@ apt-get install zsh -y
 git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
 
 # Copy Robby's .zshrc template over.
-cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
+cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc-template
 
-# Clone my files.
-git clone git@github.com:JerseyMilker/HomesteadAfter.git /home/vagrant/.homestead-after
+# Clone my .zshrc and theme.
+git clone git://github.com/JerseyMilker/HomesteadAfter.git /home/vagrant/.homestead-after
+cp /home/vagrant/.homestead-after/jersey.zsh-theme /home/vagrant/.oh-my-zsh/themes
+#cp /home/vagrant/.homestead-after/.zshrc /home/vagrant/.zshrc
 
 # Set default shell.
 chsh -s /usr/bin/zsh vagrant
