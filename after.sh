c6e5a76bd303da40bc9e4468eda6f89f6660a928
Jesse Leite <jesseleite@gmail.com>
2015-02-28 22:30:15 -0500

    Nothing much.

--- after.sh
+++ after.sh
@@ -4,8 +4,8 @@ apt-get install zsh -y
 # Clone Oh-My-Zsh.
 git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
 
-# Copy Robby's .zshrc over as .zshrc-robby for reference purposes only.
-cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc-robby
+# Copy Robby's .zshrc over as .zshrc-template for reference purposes only.
+cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc-template
 
 # Clone HomesteadAfter files.
 git clone git://github.com/JerseyMilker/HomesteadAfter.git /home/vagrant/.homestead-after
