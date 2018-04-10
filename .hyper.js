c90543f00d721787231cfdae9855f1dcd014a2aa
Jesse Leite <jesse@thisvessel.co>
2018-04-10 14:48:19 -0400

    Putting custom hyper settings back.

--- .hyper.js
+++ .hyper.js
@@ -6,13 +6,13 @@ module.exports = {
   config: {
     // choose either `'stable'` for receiving highly polished,
     // or `'canary'` for less polished but more frequent updates
-    updateChannel: 'stable',
+    updateChannel: 'canary',
 
     // default font size in pixels for all tabs
-    fontSize: 12,
+    fontSize: 14,
 
     // font family with optional fallbacks
-    fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: '"Menlo LG100", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
@@ -21,7 +21,7 @@ module.exports = {
     fontWeightBold: 'bold',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: 'rgba(248,28,229,0.8)',
+    cursorColor: 'rgba(255,255,255,0.6)',
 
     // terminal text color under BLOCK cursor
     cursorAccentColor: '#000',
@@ -33,11 +33,11 @@ module.exports = {
     cursorBlink: false,
 
     // color of the text
-    foregroundColor: '#fff',
+    foregroundColor: '#C0C2C7',
 
     // terminal background color
     // opacity is only supported on macOS
-    backgroundColor: '#000',
+    backgroundColor: 'rgba(0, 0, 0, 0.9)',
 
     // terminal selection color
     selectionColor: 'rgba(248,28,229,0.3)',
@@ -61,28 +61,28 @@ module.exports = {
     showWindowControls: '',
 
     // custom padding (CSS format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
+    padding: '18px 12px 15px 20px',
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
     // an array here instead of a color map object
     colors: {
-      black: '#000000',
-      red: '#C51E14',
-      green: '#1DC121',
-      yellow: '#C7C329',
-      blue: '#0A2FC4',
-      magenta: '#C839C5',
-      cyan: '#20C5C6',
-      white: '#C7C7C7',
-      lightBlack: '#686868',
-      lightRed: '#FD6F6B',
-      lightGreen: '#67F86F',
-      lightYellow: '#FFFA72',
-      lightBlue: '#6A76FB',
-      lightMagenta: '#FD7CFC',
-      lightCyan: '#68FDFE',
-      lightWhite: '#FFFFFF',
+      black: '#212121',
+      red: '#FC3458',
+      green: '#67B252',
+      yellow: '#E0C646',
+      blue: '#43A4CF',
+      magenta: '#E96721',
+      cyan: '#43A4CF',
+      white: '#C0C2C7',
+      lightBlack: '#424242',
+      lightRed: '#E22543',
+      lightGreen: '#67B252',
+      lightYellow: '#E6CE57',
+      lightBlue: '#43A4CF',
+      lightMagenta: '#E96721',
+      lightCyan: '#43A4CF',
+      lightWhite: '#FFFFFF'
     },
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
@@ -123,6 +123,14 @@ module.exports = {
     // bellSoundURL: 'http://example.com/bell.mp3',
 
     // for advanced config flags please refer to https://hyper.is/#cfg
+
+    modifierKeys: { cmdIsMeta: true, altIsMeta: true },
+
+    summon: {
+      hotkey: 'Command+`',
+      hideDock: false,
+      hideOnBlur: false,
+    },
   },
 
   // a list of plugins to fetch and install from npm
@@ -131,7 +139,10 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    'hyperterm-summon',
+    'hyper-stylesheet',
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
