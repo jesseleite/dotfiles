1f5d7f6203c807d0659f97b8614ee12e78f0585c
Jesse Leite <jesseleite@gmail.com>
2018-09-05 19:03:27 -0400

    Headbangin'

--- install.sh
+++ install.sh
@@ -22,6 +22,9 @@ brew bundle
 brew link --force mysql@5.7
 brew services start mysql@5.7
 
+# If still getting socket errors, follow instructions here:
+# https://superuser.com/questions/1333504/brew-install-mysql5-7-cant-connect-to-local-mysql-server-through-socket
+
 # Configure Git.
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/.gitignore_global ~/.gitignore_global
