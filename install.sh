9ab35f02197de736131f66e24bf5542eccbfd625
Jesse Leite <jesseleite@gmail.com>
2018-11-01 10:43:11 -0400

    Bring back anywhere.

--- install.sh
+++ install.sh
@@ -2,9 +2,12 @@
 
 # Step 1: Generate/paste SSH keys.
 # Step 2: Clone this repo to ~/.dotfiles.
-# Step 3: Run this installer <3
+# Step 3: Sudo run this installer <3
 
 echo "Setting up your Mac..."
+
+# Reinstate the 'anywhere' setting in gatekeeper for apps with unidentified developers.
+spctl --master-disable
 
 # Install Brew.
 if test ! $(which brew); then
