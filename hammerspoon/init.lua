b4340955f4f20eb885539696354d2f3c93fae09e
Jesse Leite <jesseleite@gmail.com>
2018-12-09 17:52:45 -0500

    Cleanup.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,14 +1,14 @@
--- Define Hyper Modifiers
+--------------------------------------------------------------------------------
+-- Global Hammerspoon Config
+--------------------------------------------------------------------------------
+
 hyper = {"cmd", "alt", "ctrl"}
 lilHyper = {"cmd", "ctrl"}
 bigHyper = {"cmd", "alt", "ctrl", "shift"}
 
--- Hammerspoon Meta Mappings
 hs.hotkey.bind(hyper, 'r', hs.reload)
 hs.hotkey.bind(hyper, 'c', hs.toggleConsole)
 
--- Spoon Installer?
--- hs.loadSpoon("SpoonInstall")
 
 -- Wicked ideas from guys I should learn from...
 -- https://aaronlasseigne.com/2016/02/16/switching-from-slate-to-hammerspoon/
@@ -16,46 +16,62 @@ hs.hotkey.bind(hyper, 'c', hs.toggleConsole)
 -- https://wincent.com/wiki/Hammerspoon
 
 
------------------------------------------------
+--------------------------------------------------------------------------------
 -- Global Window Management Settings
------------------------------------------------
+--------------------------------------------------------------------------------
 
 hs.window.animationDuration = 0
 hs.grid.setGrid('30x20')
 hs.grid.setMargins({x=16, y=16})
 
 positions = {
+
   spacious = {
-    center       = {x=5, y=3, w=15, h=12},
-    left         = {x=4, y=2, w=10, h=16},
-    right        = {x=16, y=2, w=10, h=16},
+    center       = '5,3 15x12',
+    left         = '4,2 10x16',
+    right        = '16,2 10x16',
   },
+
   thirds = {
-    left         = {x=0, y=0, w=10, h=20},
-    leftTop      = {x=0, y=0, w=10, h=10},
-    leftBottom   = {x=0, y=10, w=10, h=10},
-    center       = {x=10, y=0, w=10, h=20},
-    centerTop    = {x=10, y=0, w=10, h=10},
-    centerBottom = {x=10, y=10, w=10, h=10},
-    right        = {x=20, y=0, w=10, h=20},
-    rightTop     = {x=20, y=0, w=10, h=10},
-    rightBottom  = {x=20, y=10, w=10, h=10},
+    left         = '0,0 10x20',
+    leftTop      = '0,0 10x10',
+    leftBottom   = '0,10 10x10',
+    center       = '10,0 10x20',
+    centerTop    = '10,0 10x10',
+    centerBottom = '10,10 10x10',
+    right        = '20,0 10x20',
+    rightTop     = '20,0 10x10',
+    rightBottom  = '20,10 10x10',
   },
+
+  halves = {
+    left         = '0,0 15x20',
+    right        = '15,0 15x20',
+  },
+
 }
 
 
------------------------------------------------
+--------------------------------------------------------------------------------
+-- Window Movements
+--------------------------------------------------------------------------------
+hs.hotkey.bind(lilHyper, 's', function ()
+  snap()
+end)
+
+
+--------------------------------------------------------------------------------
 -- Multi Window Layouts
------------------------------------------------
+--------------------------------------------------------------------------------
 
 -- Just editor and browser
-hs.hotkey.bind(lilHyper, '1', function()
+hs.hotkey.bind(hyper, '1', function()
   moveApp('Hyper', positions.spacious.left)
   moveApp('Google Chrome', positions.spacious.right)
 end)
 
 -- All the things
-hs.hotkey.bind(lilHyper, '2', function()
+hs.hotkey.bind(hyper, '2', function()
   moveApp('Hyper', positions.thirds.center)
   moveApp('Google Chrome', positions.thirds.right)
   moveApp('GitHub Desktop', positions.thirds.center)
@@ -64,9 +80,9 @@ hs.hotkey.bind(lilHyper, '2', function()
 end)
 
 
------------------------------------------------
--- Focus Window
------------------------------------------------
+--------------------------------------------------------------------------------
+-- Switch Focus
+--------------------------------------------------------------------------------
 
 hs.hotkey.bind(lilHyper, 'k', function()
   hs.window.focusedWindow():focusWindowNorth()
@@ -84,16 +100,10 @@ hs.hotkey.bind(lilHyper, 'h', function()
   hs.window.focusedWindow():focusWindowWest()
 end)
 
-hs.hotkey.bind(lilHyper, 's', function()
-  local window = hs.window.focusedWindow()
-  hs.grid.snap(window)
-  print(hs.inspect(hs.grid.get(window)))
-end)
 
-
------------------------------------------------
+--------------------------------------------------------------------------------
 -- Helpers
------------------------------------------------
+--------------------------------------------------------------------------------
 
 function moveApp(application, cell)
   local window = hs.window.find(application)
@@ -102,9 +112,15 @@ function moveApp(application, cell)
   end
 end
 
+function snap()
+  local window = hs.window.focusedWindow()
+  hs.grid.snap(window)
+  print(hs.inspect(hs.grid.get(window)))
+end
 
------------------------------------------------
+
+--------------------------------------------------------------------------------
 -- The End
------------------------------------------------
+--------------------------------------------------------------------------------
 
 hs.notify.show('Hammerspoon loaded', '', '...more like hammerspork')
