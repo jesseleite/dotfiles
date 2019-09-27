decd98465be90bdb33f873ac27d4f3e41a0f32de
Jesse Leite <jesseleite@gmail.com>
2019-09-27 01:02:44 -0400

    Finish porting install.sh 🙌

--- install.sh
+++
@@ -1,6 +0,0 @@
-# Link and start mysql@5.7 whenever mac starts.
-brew link --force mysql@5.7
-brew services start mysql@5.7
-
-# If still getting socket errors, follow instructions here:
-# https://superuser.com/questions/1333504/brew-install-mysql5-7-cant-connect-to-local-mysql-server-through-socket
