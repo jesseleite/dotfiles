02c574adeecad7417f70c9ac51dac35ad0836c9e
Jesse Leite <jesseleite@gmail.com>
2020-05-20 13:21:58 -0400

    I've repeated this process too many times.

--- zsh/local/statamic.zsh
+++ zsh/local/statamic.zsh
@@ -19,11 +19,32 @@ alias plsjlo="cp ~/.dotfiles/statamic/jlo.yaml site/users/jlo.yaml"
 plscomp() { ( cd statamic && comp $* ) }
 
 # Symlink local statamic packages, no matter how they are composer required
-sym() {
+plslink() {
   if [ -n "$1" ]; then
     rm -rf vendor/statamic/$1
     ln -s ~/Code/$1 vendor/statamic/$1
   fi
 
   la vendor/statamic
+}
+
+# Setup a fresh statamic app, clear site, and install migrator
+plsmig() {
+  if [ -z "$1" ]; then
+    echo 'Please specify repo name to create for your migration!'
+    return
+  fi
+
+  cd ~/Code
+  composer create-project statamic/statamic $1 --prefer-dist --stability=dev
+  cd $1
+  gin
+  composer require statamic/migrator
+  gcam 'Require migrator.'
+  plslink cms
+  plslink migrator
+  php please site:clear -n
+  gcam 'Clear site.'
+  open .
+  echo "\nGo forth and migrate!"
 }
