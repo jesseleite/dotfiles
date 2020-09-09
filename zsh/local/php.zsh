1e02c3c79364461e8669685101631cce6f287af9
Jesse Leite <jesseleite@gmail.com>
2020-09-09 11:21:11 -0400

    Add function to set/reset php xdebug config and memory_limit.

--- zsh/local/php.zsh
+++ zsh/local/php.zsh
@@ -29,3 +29,21 @@ phpv() {
   sed -i "" "s/memory_limit.*=.*M/memory_limit\ =\ 2G/" $(php --ini | ag "/.*/php-memory-limits.ini")
   valet restart
 }
+
+phpc() {
+  local confd
+  confd=$(php --ini | ag -o "[^\s]*conf\.d[^/]")
+  if [ -z "$1" ]; then
+    cd $confd
+    return
+  fi
+  if [ "$1" = 'xdebug' ] || [ "$1" = 'x' ]; then
+    [ -f "$confd/ext-xdebug.ini" ] && rm "$confd/ext-xdebug.ini"
+    [ -f "$confd/ext-xdebug.ini.disabled" ] && rm "$confd/ext-xdebug.ini.disabled"
+    cp ~/.dotfiles/php/ext-xdebug.ini $confd
+    echo "Copied ext-xdebug.ini"
+  elif [ "$1" = 'memory' ] || [ "$1" = 'mem' ] || [ "$1" = 'm' ]; then
+    sed -i "" "s/memory_limit.*=.*/memory_limit\ =\ 2G/" "$confd/php-memory-limits.ini"
+    echo "Updated memory_limit to 2G"
+  fi
+}
