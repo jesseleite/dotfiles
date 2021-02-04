2772012c63856e240231b975bb066326f465e7cc
Jesse Leite <jesseleite@gmail.com>
2021-02-03 20:35:14 -0500

    Remove hub, install new gh cli.

--- zsh/local/git.zsh
+++ zsh/local/git.zsh
@@ -6,10 +6,10 @@ alias gpom="gl origin master"
 alias nah="grhh && gclean"
 alias gin="git init && gaa && gcmsg 'Initial commit.'"
 alias gundo="git reset HEAD^"
-alias gh="github"
 alias tower="gittower ."
 alias gdb='git remote show origin | grep "HEAD branch" | cut -d " " -f5'
 alias gcod='gco $(gdb)'
+alias gpr='gh pr checkout'
 
 # Unalias oh-my-zsh aliases, in favour of following functions
 unalias gst
