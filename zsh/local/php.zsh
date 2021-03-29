1f0a5f4b69655941a6978d50391cff6a733b7664
Jesse Leite <jesseleite@gmail.com>
2021-03-29 14:54:37 -0400

    Replace `xdebug-toggler` and old `phpv()` function with phpmon.

--- zsh/local/php.zsh
+++ zsh/local/php.zsh
@@ -5,45 +5,29 @@
 alias psr1="php-cs-fixer fix --level=psr1"
 alias psr2="php-cs-fixer fix --level=psr2"
 
-
-# ------------------------------------------------------------------------------
-# Switch PHP versions with less migraine?
-# ------------------------------------------------------------------------------
-
-INSTALLED_PHP_VERSIONS=('php@7.2' 'php@7.3' 'php@7.4' 'php')
-
-phpv() {
-  if [ -z "$1" ]; then
-    echo 'Please specify desired PHP version!'
-    return
-  fi
-  sudo --validate
-  echo "Stopping all php services..."
-  for version in $INSTALLED_PHP_VERSIONS; do
-    sudo brew services stop $version
-  done
-  composer global update
-  version="php@$(sed "s/php@//" <<< "$1")"
-  version=$(sed "s/php@8.0/php/" <<< "$version")
-  valet use $version
-  echo "Updating memory_limit to 2G..."
-  sed -i "" "s/memory_limit.*=.*M/memory_limit\ =\ 2G/" $(php --ini | ag "/.*/php-memory-limits.ini")
-  valet restart
-}
-
+# Configure php settings and extensions for current version
 phpc() {
   local confd
   confd=$(php --ini | ag -o "[^\s]*conf\.d[^/]")
+
   if [ -z "$1" ]; then
     cd $confd
     return
   fi
-  if [ "$1" = 'xdebug' ] || [ "$1" = 'x' ]; then
+
+  if [ "$1" = 'all' ] || [ "$1" = 'redis' ]; then
+    sudo pecl install redis
+  fi
+
+  if [ "$1" = 'all' ] || [ "$1" = 'xdebug' ]; then
+    sudo pecl install xdebug
     [ -f "$confd/ext-xdebug.ini" ] && rm "$confd/ext-xdebug.ini"
     [ -f "$confd/ext-xdebug.ini.disabled" ] && rm "$confd/ext-xdebug.ini.disabled"
     cp ~/.dotfiles/php/ext-xdebug.ini $confd
     echo "Copied ext-xdebug.ini"
-  elif [ "$1" = 'memory' ] || [ "$1" = 'mem' ] || [ "$1" = 'm' ]; then
+  fi
+
+  if [ "$1" = 'all' ] || [ "$1" = 'memory' ]; then
     sed -i "" "s/memory_limit.*=.*/memory_limit\ =\ 2G/" "$confd/php-memory-limits.ini"
     echo "Updated memory_limit to 2G"
   fi
