ef2865ee2f7b852b62ee40700f1171c9f5c6b9a8
Jesse Leite <jesseleite@gmail.com>
2021-11-10 17:58:22 -0500

    It's easier to `v dot` and telescope.

--- zsh/local/system.zsh
+++ zsh/local/system.zsh
@@ -2,15 +2,6 @@
 # Aliases and functions for system configuration
 # ------------------------------------------------------------------------------
 
-# Edit specific config files
-alias zshrc="z dot && vim ./zsh/zshrc"
-alias hyperjs="z dot && vim ./hyper/hyper.js"
-alias hypercss="z dot && vim ./hyper/hyper.css"
-alias vimrc="z dot && vim ./vim/vimrc"
-alias vmap="z dot && vim ./vim/mappings.vim"
-alias vplug="z dot && vim ./vim/plugins.vim"
-alias gitconfig="z dot && vim ./git/gitconfig"
-
 # Configure git user
 alias gituser="bash ~/.dotfiles/git/user.sh"
 
