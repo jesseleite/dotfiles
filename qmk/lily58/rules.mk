ddf2655bbcf52a2031702ad131ce26474641666b
Jesse Leite <jesseleite@gmail.com>
2022-02-16 01:39:51 -0500

    Use dfu bootloader and disable oled.

--- qmk/lily58/rules.mk
+++ qmk/lily58/rules.mk
@@ -1,3 +1,4 @@
+BOOTLOADER = atmel-dfu      # Required for Elite-C microcontrollers
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = no        # Audio control and System control
@@ -6,9 +7,9 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-OLED_ENABLE= yes     # OLED display
+OLED_ENABLE = no            # OLED display
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
