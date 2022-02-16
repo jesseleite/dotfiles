54fb18906f60ace05a95b3e87d9a24141dbbd598
Jesse Leite <jesseleite@gmail.com>
2022-02-16 03:14:59 -0500

    Make mod tap a bit more forgiving.

--- qmk/lily58/config.h
+++ qmk/lily58/config.h
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+#define TAPPING_TERM 150
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
