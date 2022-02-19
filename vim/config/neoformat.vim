086c6677d5a8a9113bc121f725838cedf3e2f8eb
Jesse Leite <jesseleite@gmail.com>
2022-02-19 18:39:56 -0500

    Antlers formatter now looks for `.antlers.format.json` at project root (thank you @JohnathonKoster!)

--- vim/config/neoformat.vim
+++ vim/config/neoformat.vim
@@ -4,7 +4,7 @@
 
 let g:neoformat_html_antlersformat = {
   \ 'exe': 'antlersformat',
-  \ 'args': ['format', '--o', '~/.dotfiles/statamic/antlersformat.json'],
+  \ 'args': ['format'],
   \ 'replace': 1,
   \ }
 
--- zsh/local/statamic.zsh
+++ zsh/local/statamic.zsh
@@ -12,6 +12,7 @@ alias cmst="in cms shtuff as test"
 alias cmsw="in cms npm run watch"
 alias plsuser="cp ~/.dotfiles/statamic/jesseleite@example.com.yaml users/jesseleite@example.com.yaml && echo 'User created.'"
 alias plspro="sed -i \"\" \"s/'pro'\ =>\ false/'pro'\ =>\ true/\" config/statamic/editions.php && echo 'Pro enabled.'"
+alias plsant="cp ~/.dotfiles/statamic/antlers.format.json .antlers.format.json && echo 'Antlers formatting rules copied.'"
 
 # V2
 alias plsc="pls clear:cache && pls clear:stache && pls clear:static && pls clear:glide"
