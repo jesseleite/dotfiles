ac5762304d5990b3b4e7858048bdba58fd55780c
Jesse Leite <jesseleite@gmail.com>
2022-03-10 11:33:49 -0500

    Tmux wip.

--- zsh/local/tmux.zsh
+++ zsh/local/tmux.zsh
@@ -12,13 +12,31 @@ ta() {
     return
   fi
 
-  tmux ls && read tmux_session && tmux attach -t ${tmux_session:-werk} || tmux new -s ${tmux_session:-werk}
+  tmux ls && read tmux_session && tmux attach -t ${tmux_session:-misc} || tmux new -s ${tmux_session:-misc}
 }
 
 # Ensure attached to session when opening new terminal windows
 if [ -z "$TMUX" ]; then
   ta
 fi
+
+# Open or create tmux session with z argument
+t() {
+  if [ -z "$1" ]; then
+    ta
+    return
+  fi
+
+  local sesh=$(basename $(z -e $1) | tr . _)
+
+  if [ -z $TMUX ]; then
+    (z $1 && tmux new -A -s $sesh)
+  elif tmux has-session -t=$sesh 2> /dev/null; then
+    (tmux switch -t $sesh)
+  else
+    (z $1 && tmux new -A -s $sesh -d && tmux switch -t $sesh)
+  fi
+}
 
 
 # ------------------------------------------------------------------------------
