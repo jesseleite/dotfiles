392d18f09c59a8229204ffbde778ff9e1bd90fdf
Jesse Leite <jesseleite@gmail.com>
2022-08-10 17:06:59 -0400

    Rename folder.

--- hammerspoon/window.lua
+++ hammerspoon/window.lua
@@ -1,6 +1,8 @@
-require('windows.resizer')
-require('windows.regular')
-require('windows.focus')
-require('windows.common')
-require('windows.subscriptions')
-require('windows.modes')
+require('window.resizer')
+require('window.regular')
+require('window.focus')
+require('window.common')
+require('window.subscriptions')
+require('window.modes')
+
+
