31922924b0f4ad7e8f6b0fa03ee2954dc00e3b50
Jesse Leite <jesseleite@gmail.com>
2022-08-25 09:29:28 -0400

    Sync up new subscriptions changes too.

--- hammerspoon/window/subscriptions.lua
+++ hammerspoon/window/subscriptions.lua
@@ -17,3 +17,41 @@ hs.window.filter.new():subscribe(hs.window.filter.windowCreated, function(win)
         addWindowToLayoutCell(win)
     end
 end)
+
+-- Automatically move windows to a configured position when showing them.
+-- (Only works when assigning to a variablem)
+appwatcher = hs.application.watcher.new(function(appName, event, app)
+    local handlers = {
+        [hs.application.watcher.launched] = handleAppVisible,
+        [hs.application.watcher.unhidden] = handleAppVisible,
+    }
+    if handlers[event] then handlers[event](app, app:name()) end
+end):start()
+
+function handleAppVisible(app, appName)
+    local config = apps[appName]
+    if not config then return end
+    local windows = app:visibleWindows()
+
+    -- Don't move windows if there are more than one. It's awkward.
+    -- Maybe eventually check to see if all the windows are in the same position
+    -- in which case it might make sense to reposition them all then.
+    if #windows ~= 1 then return end
+
+    for _, win in pairs(windows) do
+        -- Position the window if it's not already in the layout.
+        if not currentLayout.windows[win:id()] then
+            local position = nil
+
+            -- If the app should be in the layout, place it there.
+            if currentLayout.preset.apps[appName] then
+                local cell = currentLayout.preset.apps[appName].cell
+                position = currentLayout.preset.cells[cell][currentLayoutConfiguration]
+            elseif config.position then
+                position = config.position
+            end
+
+            if position then positionWindowUsingGrid(win, position) end
+        end
+    end
+end
