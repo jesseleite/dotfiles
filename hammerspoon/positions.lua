b689525da7b5003c7339a4ca7b5dca49698942b3
Jesse Leite <jesseleite@gmail.com>
2023-07-18 13:06:28 -0400

    Fix positions.

--- hammerspoon/positions.lua
+++ hammerspoon/positions.lua
@@ -1,37 +1,37 @@
 return {
-  full     = '0,0 40x20',
+  full     = '0,0 60x20',
 
   center = {
-    large  = '4,1 22x18',
-    medium = '6,1 18x18',
-    small  = '8,2 14x16',
-    tiny   = '9,3 12x12',
-    mini   = '11,4 8x10',
+    large  = '6,1 48x18',
+    medium = '12,1 36x18',
+    small  = '16,2 28x16',
+    tiny   = '18,3 24x12',
+    mini   = '22,4 16x10',
   },
 
   sixths = {
     left  = '0,0 5x20',
-    right  = '25,0 5x20',
+    right  = '50,0 10x20',
   },
 
   thirds = {
-    left   = '0,0 10x20',
-    center = '10,0 10x20',
-    right  = '20,0 10x20',
+    left   = '0,0 20x20',
+    center = '20,0 20x20',
+    right  = '40,0 20x20',
   },
 
   halves = {
-    left   = '0,0 15x20',
-    right  = '15,0 15x20',
+    left   = '0,0 30x20',
+    right  = '30,0 30x20',
   },
 
   twoThirds = {
-    left   = '0,0 20x20',
-    right  = '10,0 20x20',
+    left   = '0,0 40x20',
+    right  = '20,0 40x20',
   },
 
   fiveSixths = {
-    left   = '0,0 25x20',
-    right  = '5,0 25x20',
+    left   = '0,0 50x20',
+    right  = '10,0 50x20',
   },
 }
