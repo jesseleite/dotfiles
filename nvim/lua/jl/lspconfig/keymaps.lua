26c2b53519604ae90eecdd5092ee17338aa5830e
Jesse Leite <jesseleite@gmail.com>
2024-09-05 11:59:12 -0400

    Imagine forgetting a comma for this long.

--- nvim/lua/jl/lspconfig/keymaps.lua
+++ nvim/lua/jl/lspconfig/keymaps.lua
@@ -19,8 +19,8 @@ M.setup = function ()
   vim.keymap.set('n', '<Leader>ia', vim.lsp.buf.code_action, { buffer = true })
   vim.keymap.set('n', '<Leader>ie', vim.diagnostic.open_float, { buffer = true })
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = true })
-  vim.keymap.set('n', '[d', vim.lsp.diagnostic.goto_prev { buffer = true })
-  vim.keymap.set('n', ']d', vim.lsp.diagnostic.goto_next, { buffer = true })
+  vim.keymap.set('n', '[d', function () vim.diagnostic.goto_prev({ float = false }) end, { buffer = true })
+  vim.keymap.set('n', ']d', function () vim.diagnostic.goto_next({ float = false }) end, { buffer = true })
 end
 
 return M
