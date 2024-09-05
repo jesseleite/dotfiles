293a90ad1249ee079b6d6a36066ec65f835e31a7
Jesse Leite <jesseleite@gmail.com>
2024-09-05 12:02:45 -0400

    Move Herd injected stuff to laravel.zsh

--- zsh/local/laravel.zsh
+++ zsh/local/laravel.zsh
@@ -25,3 +25,26 @@ function tink() {(
     php artisan tinker --execute="dd($1)"
   fi
 )}
+
+
+# ------------------------------------------------------------------------------
+# Laravel Herd injected stuff
+# ------------------------------------------------------------------------------
+
+# Herd injected NVM configuration
+export NVM_DIR="/Users/jesseleite/Library/Application Support/Herd/config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+[[ -f "/Applications/Herd.app/Contents/Resources/config/shell/zshrc.zsh" ]] && builtin source "/Applications/Herd.app/Contents/Resources/config/shell/zshrc.zsh"
+
+# Herd injected PHP binary.
+export PATH="/Users/jesseleite/Library/Application Support/Herd/bin/":$PATH
+
+# Herd injected PHP 8.2 configuration.
+export HERD_PHP_82_INI_SCAN_DIR="/Users/jesseleite/Library/Application Support/Herd/config/php/82/"
+
+# Herd injected PHP 8.3 configuration.
+export HERD_PHP_83_INI_SCAN_DIR="/Users/jesseleite/Library/Application Support/Herd/config/php/83/"
+
+# Herd injected PHP 7.4 configuration.
+export HERD_PHP_74_INI_SCAN_DIR="/Users/jesseleite/Library/Application Support/Herd/config/php/74/"
