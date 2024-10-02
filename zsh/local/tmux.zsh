3a654b27d64e0db8a21bda68cd6775ac8943961f
Jesse Leite <jesseleite@gmail.com>
2024-10-02 15:20:25 -0400

    Not using tmuxinator.

--- zsh/local/tmux.zsh
+++ zsh/local/tmux.zsh
@@ -41,23 +41,3 @@ t() {
     (z $1 && tmux new -A -s $sesh -d && tmux switch -t $sesh)
   fi
 }
-
-
-# ------------------------------------------------------------------------------
-# Experimenting with tmuxinator
-# ------------------------------------------------------------------------------
-
-alias tx="tmuxinator"
-
-# Unalias oh-my-zsh aliases, in favour of following functions
-unalias txs
-
-# Attempt to start tmuxinator on Statamic project using z argument, otherwise just just tmuxinator start
-txs() {
-  if [ -f "$(z -e $1)/please" ]; then
-    tmuxinator start statamic $1
-    return
-  fi
-
-  tmuxinator start $1
-}
