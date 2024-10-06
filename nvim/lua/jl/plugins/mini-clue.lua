5173c79557e44c15615e9932e5d1ba7cb08559dd
Jesse Leite <jesseleite@gmail.com>
2024-10-06 16:13:22 -0400

    Clue! (WIP…. Need to add descriptions to a bunch of my mappings now)

---
+++ nvim/lua/jl/plugins/mini-clue.lua
@@ -0,0 +1,56 @@
+--------------------------------------------------------------------------------
+-- Mini.clue: Show next key clues
+--------------------------------------------------------------------------------
+
+return {
+  'echasnovski/mini.clue',
+  config = function ()
+    local miniclue = require('mini.clue')
+
+    miniclue.setup({
+      triggers = {
+        -- Leader triggers
+        { mode = 'n', keys = '<Leader>' },
+        { mode = 'x', keys = '<Leader>' },
+
+        -- Built-in completion
+        { mode = 'i', keys = '<C-x>' },
+
+        -- `g` key
+        { mode = 'n', keys = 'g' },
+        { mode = 'x', keys = 'g' },
+
+        -- Marks
+        { mode = 'n', keys = "'" },
+        { mode = 'n', keys = '`' },
+        { mode = 'x', keys = "'" },
+        { mode = 'x', keys = '`' },
+
+        -- Registers
+        { mode = 'n', keys = '"' },
+        { mode = 'x', keys = '"' },
+        { mode = 'i', keys = '<C-r>' },
+        { mode = 'c', keys = '<C-r>' },
+
+        -- Window commands
+        { mode = 'n', keys = '<C-w>' },
+
+        -- `z` key
+        { mode = 'n', keys = 'z' },
+        { mode = 'x', keys = 'z' },
+
+        -- Unimpaired style keymaps
+        { mode = 'n', keys = '[' },
+        { mode = 'n', keys = ']' },
+      },
+      clues = {
+        miniclue.gen_clues.builtin_completion(),
+        miniclue.gen_clues.g(),
+        miniclue.gen_clues.marks(),
+        miniclue.gen_clues.registers(),
+        miniclue.gen_clues.windows(),
+        miniclue.gen_clues.z(),
+      },
+    })
+  end,
+}
