5aee4acddb2e6bd2a5bf1c22e9e306f6b86f567f
Jesse Leite <jesseleite@gmail.com>
2024-10-06 13:56:42 -0400

    Don’t need this anymore.

--- nvim/after/ftplugin/php.lua
+++ nvim/after/ftplugin/php.lua
@@ -1,5 +1,2 @@
 vim.bo.commentstring = '// %s'
 
--- Doesn't seem like this is needed anymore? Maybe not with treesitter indentation handling?
--- See :help PHP_noArrowMatching
--- vim.g.PHP_noArrowMatching = 1
