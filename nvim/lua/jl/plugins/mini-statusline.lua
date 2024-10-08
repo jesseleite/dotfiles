cec2633f3c6811bce3c4053d041be22c51acbfa9
Jesse Leite <jesseleite@gmail.com>
2024-10-07 21:46:38 -0400

    Use `lazy_require` and `opts`.

--- nvim/lua/jl/plugins/mini-statusline.lua
+++ nvim/lua/jl/plugins/mini-statusline.lua
@@ -2,19 +2,17 @@
 -- Mini.statusline: A nice minimal and configurable statusline
 --------------------------------------------------------------------------------
 
+local noirbuddy_statusline = lazy_require('noirbuddy.plugins.mini-statusline')
+
 return {
   'echasnovski/mini.statusline',
   dependencies = {
     'echasnovski/mini.icons',
   },
-  config = function()
-    local noirbuddy_statusline = require('noirbuddy.plugins.mini-statusline')
-
-    require('mini.statusline').setup {
-      content = {
-        active = noirbuddy_statusline.active,
-        inactive = noirbuddy_statusline.inactive,
-      },
-    }
-  end,
+  opts = {
+    content = {
+      active = noirbuddy_statusline.active,
+      inactive = noirbuddy_statusline.inactive,
+    },
+  },
 }
