fbbcafb2a365d35568bc3c3b813e9b5c381459c2
Jesse Leite <jesseleite@gmail.com>
2024-10-07 22:04:27 -0400

    I don't really use neovim terminal, but this was erroring.

--- nvim/after/plugin/terminal.lua
+++ nvim/after/plugin/terminal.lua
@@ -2,7 +2,7 @@
 -- Terminal Window Tweaks
 --------------------------------------------------------------------------------
 -- Teej said we can just use `after/ftplugin/term.lua` for this?
--- But `set filtype?` is empty? Is this a thing? Does this require nightly?
+-- But `set filetype?` is empty? Is this a thing? Does this require nightly?
 
 vim.api.nvim_create_autocmd('TermOpen', {
   group = vim.api.nvim_create_augroup('terminal_local', { clear = true }),
@@ -10,7 +10,6 @@ vim.api.nvim_create_autocmd('TermOpen', {
     vim.wo.number = false
     vim.wo.relativenumber = false
     vim.cmd.startinsert()
-    vim.cmd.DisableWhitespace()
     vim.keymap.set('n', '<Leader>q', ':bd!<CR>', { buffer = true, silent = true })
     vim.keymap.set('n', '<Leader>c', ':bd!<CR>', { buffer = true, silent = true })
     vim.keymap.set('n', '<C-c>', ':bd!<CR>', { buffer = true, silent = true })
