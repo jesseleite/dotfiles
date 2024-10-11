5e2123bfee7c74cd4700c5fcde63453d9bb8c947
Jesse Leite <jesseleite@gmail.com>
2024-10-11 12:39:58 -0400

    Make symbols picker default for `<Leader>s` (but methods picker default for php).

--- nvim/after/ftplugin/php.lua
+++ nvim/after/ftplugin/php.lua
@@ -6,3 +6,6 @@ vim.b.minisplitjoin_config = {
   split = { hooks_post = { hooks.add_commas } },
   join  = { hooks_post = { hooks.del_commas, hooks.pad_curly } },
 }
+
+vim.keymap.set('n', '<Leader>s', require('jl.telescope.pickers').lsp_document_methods, { buffer = true, desc = 'Telescope LSP Methods' });
+vim.keymap.set('n', '<Leader>S', require('telescope.builtin').lsp_document_symbols, { buffer = true, desc = 'Telescope LSP Symbols' });
