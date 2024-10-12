629fd8f1993ab046f8bd7dfe33422fc245cbf944
Jesse Leite <jesseleite@gmail.com>
2024-10-11 23:45:24 -0400

    Experimental branch.

--- nvim/lua/jl/plugins/tinkeray.lua
+++ nvim/lua/jl/plugins/tinkeray.lua
@@ -4,6 +4,8 @@
 
 return {
   'jesseleite/vim-tinkeray',
+  branch = 'feature/multiple-scratch-files', -- Use this experimental branch
+  event = 'VeryLazy', -- Set to always load for now, until we figure out :TinkerayOpen for multiple scratch files
   keys = {
     { '<Leader>gt', '<Plug>TinkerayOpen' },
     { '<Leader>rt', '<Plug>TinkerayRun' },
