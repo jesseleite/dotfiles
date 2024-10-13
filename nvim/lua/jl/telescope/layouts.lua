59641f5c9231eff90fd3077d9d00272c7098b6a4
Jesse Leite <jesseleite@gmail.com>
2024-10-13 15:10:07 -0400

    Tweak custom helix pilled telescope layout.

--- nvim/lua/jl/telescope/layouts.lua
+++ nvim/lua/jl/telescope/layouts.lua
@@ -7,13 +7,17 @@ local M = {}
 M.helix = function(picker, max_columns, max_lines, layout_config)
   local layout = require('telescope.pickers.layout_strategies').horizontal(picker, max_columns, max_lines, layout_config)
 
+  layout.prompt.borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└' }
+
   layout.results.line = layout.results.line - 1
   layout.results.height = layout.results.height + 1
   layout.results.borderchars = { '─', '│  ', '─', '│', '│', '│', '┘', '└' }
-  layout.prompt.borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└' }
-  layout.preview.borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└' }
   layout.results.title = ''
-  layout.preview.title = 'Preview'
+
+  if layout.preview then
+    layout.preview.borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└' }
+    layout.preview.title = 'Preview'
+  end
 
   return layout
 end
