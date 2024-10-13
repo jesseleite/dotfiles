845b9fd4ee5754b19bc23041fbbf2f6f35bb292c
Jesse Leite <jesseleite@gmail.com>
2024-10-12 20:03:40 -0400

    Neovim says to use vim.uv instead of vim.loop these days.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
--- Jesse Leite uses Neovim BTW™
+-- I use Neovim BTW™
 -- https://vimfornormalpeople.com
 -- https://jesseleite.com
 --
@@ -21,7 +21,7 @@ require('jl.keymaps')
 
 -- Ensure lazy.nvim plugin manager is installed
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
     'git',
     'clone',
@@ -35,7 +35,14 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Init lazy.nvim and let it handle everything in my plugins module
 require('lazy').setup('jl.plugins', {
-  ui = {border = 'rounded', backdrop = 100 },
-  dev = { path = '~/Code/Packages' },
-  change_detection = { notify = false },
+  ui = {
+    border = 'rounded',
+    backdrop = 100,
+  },
+  change_detection = {
+    notify = false,
+  },
+  dev = {
+    path = '~/Code/Packages',
+  },
 })
