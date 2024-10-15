b8362f5aa4fd9b037b1bc39e0bb94d85fc82dac2
Jesse Leite <jesseleite@gmail.com>
2024-10-15 19:25:28 -0400

    Toggle 'screenshot mode'. cc/ @sixlive

---
+++ nvim/after/plugin/screenshot.lua
@@ -0,0 +1,36 @@
+local enabled = false
+
+local initial = {
+  number = vim.opt.number,
+  relativenumber = vim.opt.relativenumber,
+  signcolumn = vim.opt.signcolumn,
+  cursorline = vim.opt.cursorline,
+}
+
+local flip = function (opt, enabledValue)
+  if enabled then
+    vim.opt[opt] = enabledValue
+  else
+    vim.opt[opt] = initial[opt]
+  end
+end
+
+vim.keymap.set('n', '<Leader><Leader>s', function ()
+  if enabled then
+    enabled = false
+  else
+    enabled = true
+  end
+
+  flip('number', false)
+  flip('number', false)
+  flip('relativenumber', false)
+  flip('signcolumn', 'yes:2')
+  flip('cursorline', false)
+
+  if enabled then
+    vim.diagnostic.disable()
+  else
+    vim.diagnostic.enable()
+  end
+end)
