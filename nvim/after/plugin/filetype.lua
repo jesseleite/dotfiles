57bc9139cf2ff6363d214835e9b81b6d527d0709
Jesse Leite <jesseleite@gmail.com>
2024-10-19 00:36:00 -0400

    Try out blade treesitter parser.

--- nvim/after/plugin/filetype.lua
+++ nvim/after/plugin/filetype.lua
@@ -5,14 +5,6 @@
 local force_filetype = vim.api.nvim_create_augroup('force_filetype', { clear = true })
 
 vim.api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, {
-  pattern = { '*.blade.php' },
-  group = force_filetype,
-  callback = function ()
-    vim.bo.filetype = 'php'
-  end,
-})
-
-vim.api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, {
   pattern = { '*.keymap' },
   group = force_filetype,
   callback = function ()
@@ -29,3 +21,30 @@ vim.api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, {
 --     ['.*.blade.php'] = 'php', -- Until we get proper blade parser
 --   },
 -- }
+
+
+--------------------------------------------------------------------------------
+-- Blade Treesitter Parser Hackery
+--------------------------------------------------------------------------------
+-- See: https://github.com/EmranMR/tree-sitter-blade, which is still unstable!
+-- So we have to do funky things like this to get everything to work. Then
+-- you run `:TSInstall blade`, then you add queries and injections into
+-- your Neovim config's `after/queries/blade` from the repo's docs.
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.blade = {
+  install_info = {
+    url = "https://github.com/EmranMR/tree-sitter-blade",
+    files = {"src/parser.c"},
+    branch = "main",
+  },
+  filetype = "blade"
+}
+
+vim.api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, {
+  pattern = { '*.blade.php' },
+  group = force_filetype,
+  callback = function ()
+    vim.bo.filetype = 'blade'
+  end,
+})
