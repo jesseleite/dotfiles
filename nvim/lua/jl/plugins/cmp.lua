47687498ff26cdd809e929547806b05e1e054f6d
Jesse Leite <jesseleite@gmail.com>
2024-10-20 22:47:21 -0400

    Always prioritize my custom snippets over lsp completions.

--- nvim/lua/jl/plugins/cmp.lua
+++ nvim/lua/jl/plugins/cmp.lua
@@ -35,8 +35,8 @@ return {
       }),
       sources = cmp.config.sources({
         { name = 'lazydev', group_index = 0 },
-        { name = 'nvim_lsp', group_index = 1 },
         { name = 'luasnip', group_index = 1 },
+        { name = 'nvim_lsp', group_index = 1 },
         { name = 'path', group_index = 1 },
         { name = 'buffer', group_index = 2 },
       }),
