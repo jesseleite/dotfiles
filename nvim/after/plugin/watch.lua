c7245e31383e31b85f79e73d8d7db7cf37f47f3b
Jesse Leite <jesseleite@gmail.com>
2024-10-22 10:59:05 -0400

    Add `:WatchStart` to let me watch for yaml changes while browser is focused.

--- nvim/after/plugin/watch.lua
+++ nvim/after/plugin/watch.lua
@@ -1,14 +1,36 @@
 --------------------------------------------------------------------------------
--- Watch for External File Changes
+-- Automatically Watch for External File Changes
 --------------------------------------------------------------------------------
 
--- TODO: This is causing a pause when entering q: command-line window?
---
--- vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI' }, {
---   group = vim.api.nvim_create_augroup('check_for_external_changes', { clear = true }),
---   callback = function ()
---     if vim.fn.filereadable(vim.fn.bufname('%')) then
---       vim.cmd.checktime()
---     end
---   end
--- })
+vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI' }, {
+  group = vim.api.nvim_create_augroup('check_for_external_changes', { clear = true }),
+  callback = function ()
+    if vim.fn.bufname('%') ~= '' and vim.fn.filereadable(vim.fn.bufname('%')) then
+      vim.cmd.checktime()
+    end
+  end
+})
+
+
+--------------------------------------------------------------------------------
+-- Watch When Unfocused (:WatchStart and :WatchStop)
+--------------------------------------------------------------------------------
+
+local w = vim.uv.new_fs_event()
+
+local function file_on_change(err, fname, status)
+  vim.api.nvim_command('checktime')
+  w:stop()
+  file_watch_start()
+end
+
+function file_watch_start()
+  w:start(vim.fn.expand('%'), {}, vim.schedule_wrap(function(...) file_on_change(...) end))
+end
+
+function file_watch_stop()
+  w:stop()
+end
+
+vim.api.nvim_command("command! WatchStart lua file_watch_start()")
+vim.api.nvim_command("command! WatchStop lua file_watch_stop()")
