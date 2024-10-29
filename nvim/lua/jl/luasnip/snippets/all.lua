7d21775f0f997ac5a377599826864f72c3aa9408
Jesse Leite <jesseleite@gmail.com>
2024-10-29 18:34:14 -0400

    Gotta re-think these, they get in the way when trying to jump to next in func.

--- nvim/lua/jl/luasnip/snippets/all.lua
+++ nvim/lua/jl/luasnip/snippets/all.lua
@@ -1,8 +1,8 @@
 return {
 
   -- Expand after opening parens/braces to pad pair (ie. `[ ... ]`, `{ ... }`, etc.)
-  s('(', fmt('( {} ', { i(0) })),
-  s('[', fmt('[ {} ', { i(0) })),
-  s('{', fmt('{{ {} ', { i(0) })),
+  -- s('(', fmt('( {} ', { i(0) })),
+  -- s('[', fmt('[ {} ', { i(0) })),
+  -- s('{', fmt('{{ {} ', { i(0) })),
 
 }
