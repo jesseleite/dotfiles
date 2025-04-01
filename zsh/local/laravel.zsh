240a920bc8284033d62b962f598d91afaa05e75b
Jesse Leite <jesseleite@gmail.com>
2025-04-01 13:24:13 -0400

    Herd paths.

--- zsh/local/laravel.zsh
+++ zsh/local/laravel.zsh
@@ -41,11 +41,14 @@ export NVM_DIR="/Users/jesseleite/Library/Application Support/Herd/config/nvm"
 # Herd injected PHP binary.
 export PATH="/Users/jesseleite/Library/Application Support/Herd/bin/":$PATH
 
-# Herd injected PHP 8.2 configuration.
-export HERD_PHP_82_INI_SCAN_DIR="/Users/jesseleite/Library/Application Support/Herd/config/php/82/"
+# Herd injected PHP 8.4 configuration.
+export HERD_PHP_84_INI_SCAN_DIR="/Users/jesseleite/Library/Application Support/Herd/config/php/84/"
 
 # Herd injected PHP 8.3 configuration.
 export HERD_PHP_83_INI_SCAN_DIR="/Users/jesseleite/Library/Application Support/Herd/config/php/83/"
+
+# Herd injected PHP 8.2 configuration.
+export HERD_PHP_82_INI_SCAN_DIR="/Users/jesseleite/Library/Application Support/Herd/config/php/82/"
 
 # Herd injected PHP 7.4 configuration.
 export HERD_PHP_74_INI_SCAN_DIR="/Users/jesseleite/Library/Application Support/Herd/config/php/74/"
