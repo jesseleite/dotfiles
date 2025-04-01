774f7ef95a35a456d61250e62e045175707fffc4
Jesse Leite <jesseleite@gmail.com>
2025-04-01 13:20:37 -0400

    Tele wip scope wip.

--- nvim/lua/jl/plugins/telescope.lua
+++ nvim/lua/jl/plugins/telescope.lua
@@ -3,12 +3,14 @@
 --------------------------------------------------------------------------------
 
 local t -- Telescope builtin pickers
+local e -- Telescope extensions
 local c -- Custom pickers
 
 return {
   'nvim-telescope/telescope.nvim',
   dependencies = {
     'nvim-lua/plenary.nvim',
+    'mollerhoj/telescope-recent-files.nvim', -- TODO: Find better replacement for this, as dev kinda just threw it together
     'nvim-telescope/telescope-live-grep-args.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     'nvim-telescope/telescope-ui-select.nvim',
@@ -17,12 +19,12 @@ return {
     'Telescope',
   },
   keys = {
-    { '<Leader>f', function () t.git_files() end, desc = 'Telescope Git Files' },
-    { '<Leader>F', function () t.find_files() end, desc = 'Telesscope All Files' },
+    { '<Leader>f', function () e['recent-files'].recent_files() end, desc = 'Telescope Files' },
+    { '<Leader>F', function () t.filetypes() end, desc = 'Telescope Filetypes' },
+    { '<Leader>h', function () print('JUST USE `<leader>f` YA HOSER!') end, desc = 'Telescope Files' }, -- Retrain muscle memory
+    { '<Leader>H', function () t.oldfiles() end, desc = 'Telescope All History' }, -- Change this to lowercase `h` soon
     { '<Leader>b', function () t.buffers() end, desc = 'Telescope Buffers' },
     { '<Leader>m', function () t.git_status() end, desc = 'Telescope Git Status' },
-    { '<Leader>h', function () c.project_history() end, desc = 'Telescope Project History' },
-    { '<Leader>H', function () t.oldfiles() end, desc = 'Telescope History' },
     { '<Leader>s', function () t.lsp_document_symbols() end, desc = 'Telescope LSP Symbols' },
     { '<Leader>S', function () c.lsp_document_methods() end, desc = 'Telescope LSP Methods' },
     { '<Leader>l', function () t.current_buffer_fuzzy_find() end, desc = 'Telescope Current Buffer Lines' },
@@ -30,15 +32,14 @@ return {
     { '<Leader>:', function () t.command_history() end, desc = 'Telescope Command History' },
     { '<Leader>R', function () t.pickers() end, desc = 'Telescope Resume' },
     { '<Leader>?', function () t.help_tags() end, desc = 'Telescope Help Search' },
+    { '<Leader><Leader>m', function () e.macroni.saved_macros() end, desc = 'Telescope Saved Macros', mode = { 'n', 'v' } },
     { '<Leader><Leader>v', function () c.vendor_files() end, desc = 'Telescope Vendor Files' },
-    { '<Leader><Leader>d', function () c.dotfiles() end, desc = 'Telescope Dotfiles' },
-    { '<Leader><Leader>v', function () c.nvim_dotfiles() end, desc = 'Telescope Nvim Dotfiles' },
-    { '<Leader><Leader>f', function () t.filetypes() end, desc = 'Telescope Filetypes' },
+    -- { '<Leader><Leader>d', function () c.dotfiles() end, desc = 'Telescope Dotfiles' }, -- Just use sesh?
+    -- { '<Leader><Leader>v', function () c.nvim_dotfiles() end, desc = 'Telescope Nvim Dotfiles' }, -- Just use sesh?
     { '<Leader><Leader>t', function () t.builtin() end, desc = 'Telescope Builtin Pickers' },
-    { '<Leader>/', function () require('telescope').extensions.live_grep_args.live_grep_args() end, desc = 'Telescope Live Grep Args' },
+    { '<Leader>/', function () e.live_grep_args.live_grep_args() end, desc = 'Telescope Live Grep Args' },
     { '<Leader>/', function () require('telescope-live-grep-args.shortcuts').grep_visual_selection() end, mode = 'x', desc = 'Telescope Live Grep Selection' },
     { '<Leader>*', function () require('telescope-live-grep-args.shortcuts').grep_word_under_cursor() end, desc = 'Telescope Live Grep Word' },
-    { '<Leader>M', function () require('telescope').extensions.macroni.saved_macros() end, desc = 'Telescope Saved Macros', mode = { 'n', 'v' } },
   },
   config = function ()
     local telescope = require('telescope')
@@ -72,6 +73,7 @@ return {
           '.git/',
           'resources/dist',
           'storage/framework',
+          'git/submodules',
         },
         cache_picker = {
           num_pickers = -1,
@@ -106,6 +108,11 @@ return {
         },
       },
       extensions = {
+        ['recent-files'] = {
+          prompt_title = 'Files',
+          hidden = true,
+          no_ignore = true,
+        },
         live_grep_args = {
           prompt_title = 'Live Ripgrep',
           mappings = {
@@ -122,7 +129,9 @@ return {
     telescope.load_extension('ui-select')
     telescope.load_extension('macroni')
 
+    -- Shorthand helpers for mappings in `keys` funcs
     t = require('telescope.builtin')
+    e = require('telescope').extensions
     c = require('jl.telescope.pickers')
   end
 }
