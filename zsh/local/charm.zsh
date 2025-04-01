302854c854cd567ea5a1348fff804215898701bd
Jesse Leite <jesseleite@gmail.com>
2025-04-01 13:27:37 -0400

    Don’t show gum filter help.

--- zsh/local/charm.zsh
+++ zsh/local/charm.zsh
@@ -3,5 +3,6 @@
 # ------------------------------------------------------------------------------
 
 # See `gum filter --help` for style flag / env variable options
+export GUM_FILTER_SHOW_HELP=0
 export GUM_FILTER_INDICATOR_FOREGROUND=005
 export GUM_FILTER_MATCH_FOREGROUND=002
