1379a7eb4a39f86e754cc16dd0ddbc125f9d74e6
Jesse Leite <jesseleite@gmail.com>
2025-04-09 00:57:06 -0400

    Nobody likes gofmt, everybody likes gofmt.

--- nvim/lua/jl/conform/formatters.lua
+++ nvim/lua/jl/conform/formatters.lua
@@ -5,6 +5,7 @@
 return {
   -- lua = { 'stylua' }, -- See project stylua.toml for rules. TODO: Seems to heavy handed?
   php = { 'pint' },
+  go = { 'goimports' },
   json = { 'fixjson', 'pint' },
   html = { 'antlersformat' },
 }
