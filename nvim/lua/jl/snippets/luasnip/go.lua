8196c5df1cade6b0811d2c446c273f46b5ac4636
Jesse Leite <jesseleite@gmail.com>
2025-04-09 20:44:02 -0400

    Gopher life.

--- nvim/lua/jl/snippets/luasnip/go.lua
+++ nvim/lua/jl/snippets/luasnip/go.lua
@@ -6,6 +6,9 @@ return {
   -- Method
   s('met', fmt('func ({}) {}({}) {}{{\n\t{}\n}}', { i(1, 'r Receiver'), i(2, 'Name'), i(3), i(4), i(0) })),
 
+  -- Anonymous goroutine
+  s('gor', fmt('go func() {{\n\t{}\n}}()', { i(0) })),
+
   -- For loop
   s('for', fmt('for {}; {}; {} {{\n\t{}\n}}', { i(1, 'i := 0'), i(2, 'i < 5'), i(3, 'i++'), i(0) })),
 
@@ -13,7 +16,7 @@ return {
   s('forr', fmt('for {} := range {} {{\n\t{}\n}}', { i(1, 'key, value'), i(2, 'values'), i(0) })),
 
   -- Handle err
-  s('err', fmt('if err == nil {{\n\treturn {}\n}}{}', { i(1, 'err'), i(0) })),
+  s('err', fmt('if err != nil {{\n\treturn {}\n}}{}', { i(1, 'err'), i(0) })),
 
   -- Test function
   s('test', fmt('func Test{}(t *testing.T) {{\n\t{}\n}}', { i(1, 'Name'), i(0) })),
