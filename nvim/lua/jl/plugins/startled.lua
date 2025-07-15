554ae81049ca07eefe2d8e38e410b9eb4100dcbb
Jesse Leite <jesseleite@gmail.com>
2025-07-15 08:42:22 -0400

    Swap alpha out for startled 😎

---
+++ nvim/lua/jl/plugins/startled.lua
@@ -0,0 +1,10 @@
+--------------------------------------------------------------------------------
+-- Startled: Custom static start screens for Neovim, made easy
+-------------------------------a-------------------------------------------------
+
+return {
+  'jesseleite/nvim-startled',
+  dev = true,
+  lazy = false,
+  config = true,
+}
