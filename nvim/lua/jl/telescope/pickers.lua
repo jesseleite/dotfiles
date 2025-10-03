f63dc1f856fa295872e8a7d182e60f82ba14d12c
Jesse Leite <jesseleite@gmail.com>
2025-10-02 22:18:31 -0400

    I just use sesh now.

--- nvim/lua/jl/telescope/pickers.lua
+++ nvim/lua/jl/telescope/pickers.lua
@@ -6,28 +6,6 @@ local builtin = require('telescope.builtin')
 
 local M = {}
 
--- Just use sesh?
--- M.dotfiles = function ()
---   builtin.find_files({
---     prompt_title = 'Dotfiles',
---     cwd = "$HOME/.dotfiles",
---     file_ignore_patterns = {
---       '^.git/',
---       '^git/submodules/',
---     },
---   })
--- end
---
--- M.nvim_dotfiles = function ()
---   builtin.find_files({
---     prompt_title = 'Nvim Dotfiles',
---     cwd = "$HOME/.dotfiles/nvim",
---     file_ignore_patterns = {
---       '^lazy-lock.json',
---     },
---   })
--- end
-
 M.lsp_document_methods = function ()
   builtin.lsp_document_symbols({
     prompt_title = 'LSP Document Methods',
