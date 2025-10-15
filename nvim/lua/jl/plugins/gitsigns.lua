2dfee7ad70d6366379100b4b8d7baaca4f48a553
Jesse Leite <jesseleite@gmail.com>
2025-10-15 14:31:21 -0400

    Gitsigns updates

--- nvim/lua/jl/plugins/gitsigns.lua
+++ nvim/lua/jl/plugins/gitsigns.lua
@@ -7,13 +7,18 @@ return {
   lazy = false,
   opts = {
     signs = {
-      add          = { text = '▎' },
-      change       = { text = '▎' },
-      untracked    = { text = '▎' },
-    }
+      add       = { text = '▎' },
+      change    = { text = '▎' },
+      untracked = { text = '▎' },
+    },
+    signs_staged = {
+      add       = { text = '▎' },
+      change    = { text = '▎' },
+      untracked = { text = '▎' },
+    },
   },
   keys = {
-    { '<PageDown>', function () require('gitsigns').next_hunk() end },
-    { '<PageUp>', function () require('gitsigns').prev_hunk() end },
+    { '<PageDown>', function () require('gitsigns').nav_hunk('next') end },
+    { '<PageUp>', function () require('gitsigns').nav_hunk('prev') end },
   },
 }
