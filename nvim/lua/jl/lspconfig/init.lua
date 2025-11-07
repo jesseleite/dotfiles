55c9b4a150c3e887c79feb8737441345652c11ef
Jesse Leite <jesseleite@gmail.com>
2025-11-07 10:37:03 -0500

    Make LSP messages less annoying

--- nvim/lua/jl/lspconfig/init.lua
+++ nvim/lua/jl/lspconfig/init.lua
@@ -8,6 +8,12 @@ local keymaps = require('jl.lspconfig.keymaps') -- Check this out for lsp relate
 local M = {}
 
 M.setup = function ()
+  -- Use notifications for LSP messages, so that pressing ENTER isn't required
+  -- TODO: If I use a nicer notifications plugin, do I need this?
+  vim.lsp.handlers["window/showMessage"] = function(_, result, ctx)
+    vim.notify(result.message, result.type)
+  end
+
   for server,config in pairs(servers) do
     if type(config) == 'boolean' then
       config = {}
