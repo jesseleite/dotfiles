d7a88e0e2aa5756f0f42b9fc19cde8c668b66aa0
Jesse Leite <github@jesseleite.com>
2025-12-17 00:07:16 -0500

    Fix/restore oil confirmation border

--- nvim/lua/jl/plugins/oil.lua
+++ nvim/lua/jl/plugins/oil.lua
@@ -12,17 +12,11 @@ return {
     view_options = {
       case_insensitive = true,
     },
+    confirmation = {
+      border = 'rounded',
+    },
     keymaps = {
       ['<Leader>E'] = require('jl.tree.helpers').open_nvim_tree_from_oil,
-      --   local dir = require('oil').get_current_dir()
-      --   local entry = require('oil').get_cursor_entry().name
-      --   local path = dir..entry
-      --   require('nvim-tree.api').tree.find_file({
-      --     open = true,
-      --     current_window = true,
-      --     buf = path,
-      --   })
-      -- end,
     },
   },
 }
