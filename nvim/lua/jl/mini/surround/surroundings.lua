eb296efa9d3090c7cb255dbe129396485e81e2a1
Jesse Leite <git@jesseleite.com>
2026-01-07 12:43:11 -0500

    Fix `srTT` when replacing component names with dot or dash (ie. `<My.Component>` or `<my-component>`)

--- nvim/lua/jl/mini/surround/surroundings.lua
+++ nvim/lua/jl/mini/surround/surroundings.lua
@@ -8,7 +8,7 @@ local M = {}
 -- Avoid replacing default `t` though, as we don't want this behaviour with `sa` or `sd`.
 -- See: https://github.com/nvim-mini/mini.nvim/issues/1293#issuecomment-2423827325
 M.tag_name_only = {
-  input = { '<(%w-)%f[^<%w][^<>]->.-</%1>', '^<()%w+().*</()%w+()>$' },
+  input = { '<([%w-.]-)%f[^<%w-.][^<>]->.-</%1>', '^<()[%w-.]+().*</()[%w-.]+()>$' },
   output = function()
     local tag_name = require('mini.surround').user_input('Tag name (excluding attributes)')
     if tag_name == nil then return nil end
