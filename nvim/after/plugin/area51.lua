a2739e1b1e94e0a2a335d84ee69d5e602e0ebf57
Jesse Leite <git@jesseleite.com>
2026-02-10 20:47:05 -0500

    Toggle scrollbind

--- nvim/after/plugin/area51.lua
+++ nvim/after/plugin/area51.lua
@@ -4,6 +4,20 @@
 -- This is where I put stuff that I'm either testing or embarassed about.
 -- No shame; Just aliens, UFOs, cows, and crazy vimscript shenanigans.
 
+-- Toggle scrollbind in all windows
+local function toggle_scrollbind()
+  local wins = vim.api.nvim_tabpage_list_wins(0)
+  local toggle_value = not vim.wo[wins[1]].scrollbind
+
+  for _, win in ipairs(wins) do
+    vim.wo[win].scrollbind = toggle_value
+  end
+
+  vim.notify('Scrollbind ' .. (toggle_value and 'enabled' or 'disabled'))
+end
+
+vim.keymap.set('n', '<Leader><Leader>j', toggle_scrollbind, { desc = 'Toggle scrollbind in all windows' })
+
 -- Helix mode concept
 -- vim.keymap.set({'n', 'x'}, 'w', '<Esc>vw')
 
