07a0d23f9b858f6d0bbdf0b87df91c1c11345a7c
Jesse Leite <git@jesseleite.com>
2026-03-04 16:04:04 -0500

    Explicitly define keymaps

--- nvim/lua/jl/plugins/prompt-yank.lua
+++ nvim/lua/jl/plugins/prompt-yank.lua
@@ -2,9 +2,36 @@
 -- PromptYank: Copy selection with path, line numbers, etc. for LLM prompts
 --------------------------------------------------------------------------------
 
+-- TODO: Temporary workaround for `exit_visual` not working
+local exit_visual_mode = function ()
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'nx', false)
+end
+
 return {
   "polacekpavel/prompt-yank.nvim",
+  lazy = false,
+  opts = {
+    keymaps = false,
+    exit_visual = true, -- TODO: Custom keymaps don't respect this config?
+  },
   keys = {
-    { '<Leader>y', vim.cmd.PromptYank, desc = "Yank for LLM" },
+    {
+      '<Leader>y',
+      function ()
+        require('prompt-yank').yank_selection({ from_visual = true })
+        exit_visual_mode()
+      end,
+      mode = 'v',
+      desc = "Yank selection for LLM",
+    },
+    {
+      '<Leader>Y',
+      function ()
+        require('prompt-yank').yank_diagnostics({ from_visual = true })
+        exit_visual_mode()
+      end,
+      mode = 'v',
+      desc = "Yank selection with diagnostics for LLM",
+    },
   },
 }
