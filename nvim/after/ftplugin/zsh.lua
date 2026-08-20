2ad381e39dc94d608d99c0f04060a79ee11f4789
Jesse Leite <git@jesseleite.com>
2026-08-20 10:16:31 -0400

    Zsh buffer blocks for aliases and functions

--- nvim/after/ftplugin/zsh.lua
+++ nvim/after/ftplugin/zsh.lua
@@ -1,3 +1,6 @@
 vim.bo.tabstop = 2
 vim.bo.softtabstop = 2
 vim.bo.shiftwidth = 2
+
+vim.b.current_buffer_blocks_title = 'Aliases & Functions'
+vim.b.current_buffer_blocks_query = "^alias\\  | '()\\ { | ^function\\  "
